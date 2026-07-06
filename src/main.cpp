b48f159d3520fafed07b208d109e953444d7fe13
Kshitij Das <kshitij.das1501@gmail.com>
2026-07-06 22:39:49 +0530

    Set up a loop to keep the shell running and prompting

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,17 @@ int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // TODO: Uncomment the code below to pass the first stage
-    std::cout << "$ ";
+    // Loop to keep the shell running and continuously prompting
+    while (true) 
+    {
+      std::cout << "$ ";
+      
+      // Captures the user's command in the "command" variable
+      std::string command;
+      std::getline(std::cin, command);
+  
+      // Prints the "<command>: command not found" message
+      std::cout << command << ": command not found" << std::endl;
+    }
 
-    // Captures the user's command in the "command" variable
-    std::string command;
-    std::getline(std::cin, command);
-
-    // Prints the "<command>: command not found" message
-    std::cout << command << ": command not found" << std::endl;
-    
 }
