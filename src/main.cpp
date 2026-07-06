f98a78cbbce20e11b7188259ca4c53600c68b5c5
Kshitij Das <kshitij.das1501@gmail.com>
2026-07-06 22:34:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
+
+    // Captures the user's command in the "command" variable
+    std::string command;
+    std::getline(std::cin, command);
+
+    // Prints the "<command>: command not found" message
+    std::cout << command << ": command not found" << std::endl;
+    
 }
