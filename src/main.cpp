c2042e8a5cfb6b8886ae412df4842b50274f6ebd
Kshitij Das <kshitij.das1501@gmail.com>
2026-07-07 08:08:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+int main()
+{
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
-    // Loop to keep the shell running and continuously prompting
-    while (true) 
-    {
-      std::cout << "$ ";
-      
-      // Captures the user's command in the "command" variable
-      std::string command;
-      std::getline(std::cin, command);
+  // Loop to keep the shell running and continuously prompting
+  while (true)
+  {
+    std::cout << "$ ";
 
-      // To exit program with "exit"
-      if (command == "exit") {
-          break;
-      }
-  
+    // Captures the user's command in the "command" variable
+    std::string command;
+    std::getline(std::cin, command);
+
+    // To exit program with "exit"
+    if (command == "exit") {
+      break;
+    }
+    // Echo command : To print the arguments passed by the user
+    else if (command.substr(0,5) == "echo ") {
+      std::cout << command.substr(5) << std::endl;
+    }
+    else {
       // Prints the "<command>: command not found" message
       std::cout << command << ": command not found" << std::endl;
     }
-
+  }
+  
 }
