4eb3c20251bc845e9b434e3ac9486ff9eebb7249
Kshitij Das <kshitij.das1501@gmail.com>
2026-07-09 02:37:39 +0530

    refactored complete code base into functional system | implemented till type cmd

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,68 @@
 #include <iostream>
 #include <string>
 
+
+// Logic for type command using vector to check for valid command
+const std::vector<std::string> g_valid_commands = {"exit", "echo", "type"};
+
+void commandType(std::string userInput)
+{
+	userInput.erase(0,5);
+
+	auto it = std::find(std::begin(g_valid_commands), std::end(g_valid_commands), userInput);
+
+	if (it != std::end(g_valid_commands)) {
+		std::cout << userInput << " is a shell builtin" << std::endl;
+	}
+	else {
+		std::cout << userInput << ": not found" << std::endl;
+	}
+}
+
+// Logic for echo command using the string part apart from "echo "
+void commandEcho(std::string userInput)
+{
+	userInput.erase(0,5);
+	std::cout << userInput << std::endl;
+}
+
+// Basic system of Read Evaluate and Printing for the Shell
+int REP_System()
+{
+	std::cout << "$ ";
+
+	std::string userInput;
+	std::getline(std::cin, userInput);
+
+	// Checks for appropriate user commands
+	if (userInput == "exit") {
+		return -1;
+	}
+	if (userInput.substr(0,5) == "echo ") {
+		commandEcho(userInput);
+	}
+	if (userInput.substr(0,5) == "type ") {
+		commandType(userInput);
+		return 0;
+	}
+
+	std::cout << userInput << ": command not found" << std::endl;
+	return 0;
+}
+
 int main()
 {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	// Flush after every std::cout / std:cerr
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-  // Loop to keep the shell running and continuously prompting
-  while (true)
-  {
-    std::cout << "$ ";
+	// Loop to keep the shell running and continuously prompting
+	while (1)
+	{
+		int returnVal;
+		returnVal = REP_System();
 
-    // Captures the user's command in the "command" variable
-    std::string command;
-    std::getline(std::cin, command);
-
-    // To exit program with "exit"
-    if (command == "exit") {
-      break;
-    }
-    // Echo command : To print the arguments passed by the user
-    else if (command.substr(0,5) == "echo ") {
-      std::cout << command.substr(5) << std::endl;
-    }
-    else if (command.substr(0,5) == "type ") {
-      std::string cmd = command.substr(5);
-      if (cmd == "echo" || cmd == "exit" || cmd == "type") {
-        std::cout << cmd << " is a shell builtin" << std::endl;
-      }
-      else {
-        std::cout << cmd << ": not found" << std::endl;
-      }
-    }
-    else {
-      // Prints the "<command>: command not found" message
-      std::cout << command << ": command not found" << std::endl;
-    }
-  }
-
+		// Checks for invalid running or breakage
+		if (returnVal == -1) { break; }
+	}
 }
