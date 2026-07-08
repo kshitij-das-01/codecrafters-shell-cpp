97eacd9ee581da2e39440a033988c7166aac29f5
Kshitij Das <kshitij.das1501@gmail.com>
2026-07-09 02:59:32 +0530

    PATH env variable and directory search into type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <vector>
+#include <iterator>
+#include <sstream>
+
+// To define OS based path spliter - different OS adaptible
+#if _WIN32
+constexpr char PATH_LIST_SEPARATOR = ';';
+#else
+constexpr char PATH_LIST_SEPARATOR = ':';
+#endif
 
 
 // Logic for type command using vector to check for valid command
@@ -7,14 +19,36 @@ const std::vector<std::string> g_valid_commands = {"exit", "echo", "type"};
 
 void commandType(std::string userInput)
 {
+	// Trime the input
 	userInput.erase(0,5);
 
+	// Finds the commands in the vector
 	auto it = std::find(std::begin(g_valid_commands), std::end(g_valid_commands), userInput);
 
+	// Evalutes if the command is found
 	if (it != std::end(g_valid_commands)) {
 		std::cout << userInput << " is a shell builtin" << std::endl;
 	}
-	else {
+	else 
+	{
+		// Define path related variables
+		std::string pathVar = std::getenv("PATH");
+		std::istringstream path_stream(pathVar);
+		std::string pathSplit;
+
+		// get different PATH's and put it in pathSplit | the splitting is based on the defined constant speparator above  
+		while (std::getline(path_stream, pathSplit, PATH_LIST_SEPARATOR))
+		{
+			std::string filePath = pathSplit + '/' + userInput;
+
+			if (access(filePath.c_str(), X_OK) == 0)
+			{
+				std::cout << userInput << " is "
+					<< filePath << std::endl;
+				return;
+			}
+		}
+
 		std::cout << userInput << ": not found" << std::endl;
 	}
 }
